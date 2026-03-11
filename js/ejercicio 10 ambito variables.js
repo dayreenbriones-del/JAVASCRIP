db42cce736507f6927ba60430d4f47033da7416b
JDAVID <j.david.alb6212@gmail.com>
2026-03-11 08:06:49 -0600

    p

---
+++ js/ejercicio 10 ambito variables.js
@@ -0,0 +1,29 @@
+"use strict"
+
+/* let numero = 10;
+
+function mostrarNumero(){
+   let numero = 20;
+    console.log(numero);
+}
+mostrarNumero();
+
+console.log(numero);
+console.log(); */
+
+let frase='HOLA MUNDO ';
+
+//console.log(frase);
+
+//console.log(frase.slice(4));
+
+
+/* console.log(frase.replace("HOLA", "ADIOS"));
+
+
+console.log(frase.includes("@"));
+ */
+
+//console.log(frase.indexOf("m"));
+
+console.log(frase.split(" "));
---
+++ js/practica.js
@@ -0,0 +1,6 @@
+let palabra ="ana"
+
+function invertir(palabra){
+    return palabra.split("").reverse().join("");
+}
+console.log(invertir(palabra));
