b7fdea60048ffb463e030842d7a0bd2366c13da7
JDAVID <j.david.alb6212@gmail.com>
2026-03-11 11:38:54 -0600

    pp

---
+++ js/ejercicio 11 number.js
@@ -0,0 +1,5 @@
+//metodos de la clase number
+
+let numero = 123.456789;
+console.log(numero.toFixed(2));
+console.log(numero.toExponential(3));
---
+++ js/ejercicio 12.js
@@ -0,0 +1,54 @@
+//operadores arimeticos
+/* let a = 9;
+let b = 2;
+console.log("suma: " + (a + b));
+console.log("resta: " + (a - b));
+console.log("multiplicacion: " + (a * b));
+console.log("division: " + (a % b));
+console.log("exponente: " + (a ** b));
+
+
+//operadores de signacion
+let c = 5;
+c += 3; 
+console.log("asignacion suma: " + c );
+c -= 2;
+console.log("asignacion resta: " + c );
+c *= 4;
+console.log("asignacion multiplicacion: " + c );
+c /= 2;
+console.log("asignacion division: " + c );
+c **= 2;
+console.log("asignacion exponente: " + c );
+
+
+let d = 10;
+let e = "10";
+console.log("igualdad: " + (d == e));
+console.log("identidad: " + (d === e));
+console.log("diferente: " + (d != e));
+console.log("no identico: " + (d !== e));
+console.log("mayor que: " + (d > 5));
+console.log("menor: " + (d < 15));
+ */
+
+let horas = 45;
+let pagoHora = 5000;
+let salario;
+
+if (horas <= 40) {
+    salario = horas * pagoHora;
+} else {
+    let horasExtra = horas - 40;
+    salario = (40 * pagoHora) + (horasExtra * pagoHora * 1.5);
+}
+
+console.log("Salario total: ₡" + salario);
+
+
+let celsius = 30;
+let fahrenheit;
+
+fahrenheit = (celsius * 9/5) + 32;
+
+console.log("Temperatura en Fahrenheit: " + fahrenheit);
