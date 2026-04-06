077ec1c4be4c6d33cbee18f79b6591978bd40060
JDAVID <j.david.alb6212@gmail.com>
2026-04-06 11:45:30 -0600

    dayrenn

---
+++ audio/js/codigo.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const audio = new Audio();
+
+const input = document.getElementById("seleccionar");
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+const mute = document.getElementById("mute");
+const loop = document.getElementById("loop");
+const volumen = document.getElementById("volumen");
+const velocidad = document.getElementById("velocidad");
+
+input.addEventListener("change", () => {
+    const archivo = input.files[0];
+    if (archivo) {
+        audio.src = URL.createObjectURL(archivo);
+    }
+});
+
+play.addEventListener("click", () => {
+    audio.play();
+});
+
+pause.addEventListener("click", () => {
+    audio.pause();
+});
+
+mute.addEventListener("click", () => {
+    audio.muted = !audio.muted;
+});
+
+loop.addEventListener("click", () => {
+    audio.loop = !audio.loop;
+});
+
+volumen.addEventListener("input", function () {
+    audio.volume = this.value;
+});
+
+velocidad.addEventListener("input", function () {
+    audio.playbackRate = this.value;
+});
